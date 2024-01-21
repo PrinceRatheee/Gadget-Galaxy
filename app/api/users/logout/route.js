d289518301c0b882995511a4acc8117a1c92bdd7
PrinceRatheee <rathi.prince2@gmail.com>
2024-01-21 22:21:02 +0530

    final and last commit of the project

--- app/api/users/logout/route.js
+++ app/api/users/logout/route.js
@@ -9,8 +9,9 @@ export async function GET() {
                 success: true,
             }
         )
-        response.cookies.set("token", null, { httpOnly: true });
-
+        response.cookies.set("token", "", 
+        { httpOnly: true, expires: new Date(0) 
+        });
         return response;
     } catch (error) {
         return NextResponse.json({ error: error.message }, { status: 500 });
